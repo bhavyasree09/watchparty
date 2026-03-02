8b3b8b15d3c11fb490e69e265afc0229b7decbb3
bhavyasreek <bhavyasreekandi@gmail.com>
2026-03-02 12:45:38 +0530

    updated code

--- dist/assets/react-DQH-dK0S.js
+++ dist/assets/react-DQH-dK0S.js
@@ -1,4 +1,4 @@
-import{R as O}from"./index-DKH46Wyo.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-CplUDmW5.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
+import{R as O}from"./index-tJtfJQHQ.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-CplUDmW5.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
         <script type="application/json" id="config">
           ${JSON.stringify(s.config||{})}
         <\/script>
